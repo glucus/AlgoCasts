d5610849203170a46c3a9c2e2f94db9a592dea7e
AVoskresenskaya <voskresenskayaas@gmail.com>
2022-03-23 21:35:53 +0300

    feat(*): reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt1(n) {
+    const reverse = (str) => str.split('').reverse().join('');
+
+    if (n < 0) {
+        const str = (-n).toString();
+        const reversedStr = reverse(str);
+        return -Number(reversedStr);
+    }
+    const str = n.toString();
+    const reversedStr = reverse(str);
+    return Number(reversedStr);
+}
+
+function reverseInt2(n) {
+    const reversedStr = n.toString().split('').reverse().join('');
+    return (n < 0) ? -parseInt(reversedStr) : parseInt(reversedStr);
+}
+
+function reverseInt(n) {
+    const reversedStr = n.toString().split('').reverse().join('');
+    return Math.sign(n) * parseInt(reversedStr);
+}
 
 module.exports = reverseInt;
