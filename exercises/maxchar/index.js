598540015a395c5232c3fb7491e5413ddca209bc
AVoskresenskaya <voskresenskayaas@gmail.com>
2022-03-23 23:30:53 +0300

    feat(*): maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const strMap = {};
+
+    for (let char of str) {
+        // strMap[char] = strMap[char] ? strMap[char] + 1 : 1;
+
+        if (strMap[char]) {
+            strMap[char] = strMap[char] + 1;
+        } else {
+            strMap[char] = 1;
+        }
+    }
+
+    let maxValue = 0;
+    let maxProp;
+
+    for (let prop in strMap) {
+        if (strMap[prop] > maxValue) {
+            maxValue = strMap[prop];
+            maxProp = prop;
+        }
+    }
+    return maxProp;
+}
 
 module.exports = maxChar;
