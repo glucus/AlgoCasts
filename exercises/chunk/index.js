ef78e3ac9ce8123760ed3e996c5d4206ee0207c4
AVoskresenskaya <voskresenskayaas@gmail.com>
2022-03-24 23:17:41 +0300

    feat(*): chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk1(array, size) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i + size) {
+        // array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
+        const chunk = array.splice(i, size);
+        result.push(chunk);
+    }
+    return result;
+}
+
+function chunk(array, size) {
+    let result = [];
+    let chunk = [];
+
+    array.forEach((elem, index) => {
+        if (chunk.length < size) {
+            chunk.push(elem);
+
+            if (chunk.length === size || index === array.length - 1) {
+                result.push(chunk);
+                chunk = [];
+            }
+        }
+    });
+
+    return result;
+}
 
 module.exports = chunk;
