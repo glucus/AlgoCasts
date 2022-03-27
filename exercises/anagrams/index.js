4b50f598344d5e01a03496980b76770cea9201de
AVoskresenskaya <voskresenskayaas@gmail.com>
2022-03-28 00:28:42 +0300

    feat(*): anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const createStringMap = (str) => {
+    let strMap = {};
+    const arr = str.split('');
+    arr.forEach((elem) => {
+        if (!strMap[elem]) {
+            strMap[elem] = 1;
+        } else {
+            strMap[elem]++;
+        }
+    })
+    return strMap;
+}
+
+function anagrams1(stringA, stringB) {
+    const notCharacters = /\W/g
+
+    const format = (str) => str.toLowerCase().replace(notCharacters, '');
+    const formattedStringA = format(stringA);
+    const formattedStringB = format(stringB);
+
+    if (formattedStringA.length !== formattedStringB.length) {
+        return false;
+    } else {
+        const stringMapA = createStringMap(formattedStringA);
+        const stringMapB = createStringMap(formattedStringB);
+
+        for (let prop in stringMapA) {
+            if (stringMapB[prop] !== stringMapA[prop]) return false
+        }
+        return true;
+    }
+}
+
+const anagrams = (stringA, stringB) => {
+    const formatString = (str) => str.toLowerCase().replace(/\W/g, '');
+    const sortString = (str) => str.split('').sort().join('');
+
+    const sortedStringA = sortString(formatString(stringA));
+    const sortedStringB = sortString(formatString(stringB));
+
+    return sortedStringA === sortedStringB;
+}
 
 module.exports = anagrams;
