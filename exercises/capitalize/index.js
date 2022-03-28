ac76877bcdedde41f2b905477292d97a334a7c33
AVoskresenskaya <voskresenskayaas@gmail.com>
2022-03-28 23:25:41 +0300

    feat(*): capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,39 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize1(str) {
+    const replacer = (match, p1, p2) => {
+        return `${p1.toUpperCase()}${p2}`
+    };
+
+    const capitalized = str.replace(/(\b\w)(\w*)\b/g, replacer);
+    return capitalized;
+}
+
+function capitalize2(str) {
+    const words = str.split(/\W/g);
+    const newWords = words.map(word => {
+        return word ? word[0].toUpperCase() + word.slice(1) : '';
+    });
+
+    let result = str.slice(0);
+
+    for (let i = 0; i < words.length; i++) {
+        result = result.replace(words[i], newWords[i]);
+    }
+
+    console.log(result);
+    return result;
+}
+
+function capitalize(str) {
+    const words = str.split(' ');
+    const newWords = words.map(word => {
+        return word[0].toUpperCase() + word.slice(1);
+    });
+
+    const result = newWords.join(' ');
+    return result;
+}
 
 module.exports = capitalize;
