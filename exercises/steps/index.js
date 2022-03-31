c5f44a190cd871c2efe75936883e46d8e3faa26d
AVoskresenskaya <voskresenskayaas@gmail.com>
2022-03-31 23:53:56 +0300

    feat(*): steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,56 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps1(n) {
+    for (let i = 1; i < n + 1; i++) {
+        const pounds = Array(i).fill('#').join('');
+        const spaces = Array(n - i).fill(' ').join('');
+        const result = pounds + spaces;
+        console.log(result);
+    }
+}
+
+// solution with matrix
+function steps2(n) {
+    for (let i = 0; i < n; i++) {
+        let result = '';
+
+        for (let j = 0; j < n; j++) {
+            if (j > i) {
+                result += ' ';
+            } else {
+                result += '#'
+            }
+        }
+        console.log(result);
+    }
+}
+
+function steps3(n, row = 0) {
+    if (row === n) {
+        return;
+    }
+    let str = '';
+    while (str.length < n) {
+        if (str.length <= row) {
+            str = str + '#';
+        } else str = str + ' ';
+    }
+    console.log(str);
+    steps(n, row + 1);
+}
+
+function steps(n, row = 0, str = '') {
+    if (row === n) {
+        return;
+    }
+    if (str.length === n) {
+        console.log(str);
+        steps(n, row + 1, '');
+    } else {
+        const characterToAdd = (str.length <= row) ? '#': ' ';
+        steps(n, row, str + characterToAdd);
+    }
+}
 
 module.exports = steps;
