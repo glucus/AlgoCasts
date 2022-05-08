21b6ccd955f171b0b754f97403b52acc73d338a3
Aleksandra <voskresenskayaas@gmail.com>
2022-05-08 11:05:54 +0300

    feat(fib): fibonacchi iterative

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,19 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    const result = [];
 
+    // generate each element of array and then print out the last element
+    // linear runtime (for increase in n by 1 requires calculation of 1 addition element)
+    // also, simple for loop over 1 array -> linear runtime
+    for (let i = 0; i <= n; i++) {
+        if (i === 0 || i === 1) {
+            result[i] = i;
+        } else {
+            result[i] = result[i - 1] + result[i - 2]
+        }
+    }
+    return result[n];
+}
 module.exports = fib;
